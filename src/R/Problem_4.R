7ab5d8910f621f14ae3f3af641cb711157a29c35
Richard Buractaon <richard.buractaon@soteradefense.com>
2014-12-30 18:32:09 -0800

    Problem 4 Solution

---
+++ src/R/Problem_4.R
@@ -0,0 +1,21 @@
+# Largest palindrome product
+#
+# Problem 4
+#
+# A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is
+# 9009 = 91 × 99.
+#
+# Find the largest palindrome made from the product of two 3-digit numbers.
+require('Kmisc')
+
+P <- vector()
+for (x in seq(999,100,-1)) {
+  for (y in seq(999,100,-1)) {
+    z <- x * y
+    Zstr <- as.character(z)
+    if (Zstr == str_rev(Zstr)) {
+      P <- append(P,z)
+    }
+  }
+}
+answer <- max(P)
